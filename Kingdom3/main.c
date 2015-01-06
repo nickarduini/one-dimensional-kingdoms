92c4d48da431897364f091d0251205f96f1bd6b1
Nicholas Arduini <nick1@Nicholass-MacBook-Pro.local>
2015-01-05 17:29:53 -0800

    Initial Commit

---
+++ Kingdom3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Kingdom3
+//
+//  Created by Nicholas Arduini on 1/5/15.
+//  Copyright (c) 2015 Nicholas Arduini. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
